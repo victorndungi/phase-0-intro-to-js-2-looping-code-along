2b908780dc2e27fa996882e94ae4a5781dec17aa
victorndungi <victor.ndungi@student.moringaschool.com>
2024-07-11 13:35:53 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
+
